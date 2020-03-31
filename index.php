2dd8e7fdc22f8823f284d34ba662527629c8d922
Gajemar <62676776+Gajemar@users.noreply.github.com>
2020-03-30 21:28:53 -0300

    trabalho
    então esse e o trabalho

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <html>
 <body>
-  
+  <div> 
+	<h3> O que é o GitHub?</h3> 
+	<P>E um site que permite que controlar prjetos de versonamentos de sua equipe e os seus propios projetos</ p >
+	<h2> Como criar um repositório</h2>
+	<p>Para criar um repositório no site é preciso clicar no simbolo de "+" que esta no canto superior direito e ir no opção de "New repository" logo apos você ira por o titulo do repositorio e se quiser descrever sobre o repositorio  e do que ele é,  depois escolha entre o seu repositório entre Público ou Privado e por fim vá em criar repositório e estará finalizado</p>
+	<h3> Público</h3>
+	<p>Qualquer pessoa podera ver o seu repositório, e você escolhera quem irá fazer os commits.</p>
+	<h3> Privado</h3>
+	<p>Escolhe quem ira visualizar e quem pode fazer o commit.</p>
+	<h3>Clone</h3>
+	<p>Quando criar um repositório, ele passa a existir um link para repositório remote que é possível clonar seu repositório para criar uma cópia em outro computador se for preciso poderar sincronizar entre os dois locais.</p>
+	<h3> Inserção de Arquivos</h3>
+	<p>Pse desejar inserir um arquivos, se direcione até a pagina principal do repositório e selecione "Upload files" e click "choose your files" depois selecione o arquivo do seu desejo logo em baixo você vera a "commit changes" e digite uma mensagem de confirmação e significando as altereações feitas no arquivo, abaixo e logo apos último clique em "Commit changes" que esta em verde para fazer a commit das alterações.</p>
+	<h3>commit</h3>
+	<p>commitar e permitir que uma pessoa envie um novo arquivo para o seu repositorio alterando-o</p>
+ </div>
 </body>
 </html>
